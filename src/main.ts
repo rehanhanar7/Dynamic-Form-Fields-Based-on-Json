9d5efad0ec909f85713086efe7fafb43b70ed4b7
rehanhanar7 <rehanhanar7@gmail.com>
2023-02-07 17:00:17 +0530

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
@@ -26,27 +27,28 @@ interface FieldConfig {
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   template: `
-  <form [formGroup]="form" (ngSubmit)="submit()">
+  <form [formGroup]="form" (ngSubmit)="submit()" class="p-3">
   <ng-container *ngFor="let field of fields">
-    <div [ngSwitch]="field.type">
-      <input *ngSwitchCase="'text'" [type]="field.type" [formControlName]="field.name">
-      <textarea *ngSwitchCase="'textarea'" [formControlName]="field.name"></textarea>
-      <select *ngSwitchCase="'select'" [formControlName]="field.name">
+    <div class="form-group" [ngSwitch]="field.type">
+      <label class="font-weight-bold">{{ field.name }}:</label>
+      <input *ngSwitchCase="'text'" [type]="field.type" class="form-control col-12 col-sm-6" [formControlName]="field.name">
+      <textarea *ngSwitchCase="'textarea'" class="form-control col-12 col-sm-4" [formControlName]="field.name"></textarea>
+      <select *ngSwitchCase="'select'" class="form-control col-12 col-sm-6" [formControlName]="field.name">
         <option *ngFor="let option of field.options">{{option}}</option>
       </select>
-      <div *ngSwitchCase="'radio'">
+      <div *ngSwitchCase="'radio'" class="d-flex col-12 col-sm-6">
         <div *ngFor="let option of field.options">
-          <input type="radio" [value]="option" [formControlName]="field.name"> {{option}}
+          <input type="radio" [value]="option" [formControlName]="field.name" class="mr-2"> {{option}}
         </div>
       </div>
-      <input *ngSwitchCase="'date'" [type]="field.type" [formControlName]="field.name">
+      <input *ngSwitchCase="'date'" [type]="field.type" class="form-control col-12 col-sm-6" [formControlName]="field.name">
     </div>
-    <div *ngIf="form.get(field.name).invalid && form.get(field.name).touched">
+    <div class="form-group" *ngIf="form.get(field.name) && form.get(field.name).invalid && form.get(field.name).touched">
       <div *ngIf="form.get(field.name).errors.required">{{field.name}} is required</div>
       <div *ngIf="form.get(field.name).errors.pattern">{{field.name}} does not match the pattern</div>
     </div>
   </ng-container>
-  <button type="submit" [disabled]="form.invalid">Submit</button>
+  <button type="submit" class="btn btn-primary mt-3" [disabled]="form.invalid">Submit</button>
 </form>
   `,
 })
@@ -95,23 +97,28 @@ export class App implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({});
     this.fields.forEach((field) => {
+      this.form.addControl(field.name, new FormControl('', field.validators));
+    });
+    this.fields.forEach((field) => {
       if (field.type === 'select' && field.optionsUrl) {
         // Get the options for this field from the API
-        this.http.get(field.optionsUrl).subscribe((options: any[]) => {
-          field.options = options.map((item) => {
-            const prop = field.objectProp.split('.');
-            const value = prop.reduce((obj, key) => {
-              return obj ? obj[key] : null;
-            }, item);
-            return value;
-          });
-          this.form.addControl(
-            field.name,
-            this.fb.control({ value: field.options[0], disabled: false }, [
-              Validators.required,
-            ])
-          );
-        });
+        this.http.get(field.optionsUrl).subscribe(
+          (options: any[]) => {
+            field.options = options.map((item) => {
+              const prop = field.objectProp.split('.');
+              const value = prop.reduce((obj, key) => {
+                return obj ? obj[key] : null;
+              }, item);
+              return value;
+            });
+          },
+          () => {
+            this.form.addControl(
+              field.name,
+              this.fb.control('', [Validators.required])
+            );
+          }
+        );
       } else {
         this.form.addControl(
           field.name,
